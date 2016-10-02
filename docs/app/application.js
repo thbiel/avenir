4547360c8dae246e588589b010020f119bf3c86b
Tor Håvard Bieltvedt <th@bieltvedt.net>
2016-10-02 15:15:06 +0200

    Bootstrap responsive design + misc.

--- docs/app/application.js
+++ docs/app/application.js
@@ -1,3 +1,7 @@
+---
+    # Front Matter enabled
+---
+
 var app = angular.module('app', ['ngRoute']);
 
 
@@ -113,7 +117,7 @@ var pages = [
 
 
 function isAuthenticated() {
-    return authenticatedUser
+    return !authenticatedUser
 }
 
 var authenticatedUser;
@@ -151,21 +155,9 @@ function resolvePage(routeParams) {
  * Images
  */
 var images = [
-    {id: 'image-08', uri: 'images/tor-haavard/IMG_1495.jpg'},
-    {id: 'image-07', uri: 'images/tor-haavard/IMG_1492.jpg'},
-    {id: 'image-06', uri: 'images/tor-haavard/IMG_1491.jpg'},
-    {id: 'image-15', uri: 'images/tor-haavard/IMG_1593-2.jpg'},
-    {id: 'image-01', uri: 'images/tor-haavard/30248-269.jpg'},
-    {id: 'image-02', uri: 'images/tor-haavard/IMG_0423.jpg'},
-    {id: 'image-03', uri: 'images/tor-haavard/IMG_0428.jpg'},
-    {id: 'image-04', uri: 'images/tor-haavard/IMG_1474.jpg'},
-    {id: 'image-05', uri: 'images/tor-haavard/IMG_1479.jpg'},
-    {id: 'image-09', uri: 'images/tor-haavard/IMG_1513.jpg'},
-    {id: 'image-10', uri: 'images/tor-haavard/IMG_1522.jpg'},
-    {id: 'image-11', uri: 'images/tor-haavard/IMG_1526.jpg'},
-    {id: 'image-12', uri: 'images/tor-haavard/IMG_1545.jpg'},
-    {id: 'image-13', uri: 'images/tor-haavard/IMG_1548.jpg'},
-    {id: 'image-14', uri: 'images/tor-haavard/IMG_1576.jpg'}
+{% for image in site.data.images.images %}
+    {id: '{{image.id}}', uri: '{{image.uri}}'},
+{% endfor %}
 ];
 
 
